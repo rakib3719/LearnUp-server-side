31541a0714ade59f5c00112540dbc6558fbb0bb7
rakib3719 <bannah76769@gmail.com>
2024-05-10 08:52:42 +0600

    mongodb connect to server side

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000;
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(cors({
@@ -20,17 +21,41 @@ app.use(express.json())
 app.use(cookieParser())
 
 
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ngsjczb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res)=>{
 
     res.send('assinment 11 serverside ready to work')
 })
 
-app.post('/jwt', (req, res)=>{
 
-const user = req.body;
-const token = jwt.sign(user, )
-
-})
 app.listen(port, ()=>{
 
 console.log(`this port is ${port}`);
